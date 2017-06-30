600ac469c5ce32375a30e7b176506142bc1693ea
Pavel Serikov <pavel.p.serikov@gmail.com>
2017-07-01 01:55:46 +0300

    Changes some docs, fixed Mojolicious::Plugin::Webtail dependency, fixed pavelsr/camshotbot Dockerfile

--- README.md
+++ README.md
@@ -8,11 +8,13 @@ version 0.01
 
 # ENVIRONMENT VARIABLES
 
-To get list of all environment variables:
+Environment variables are always checked firstly, before any config files
+
+To get list of all available environment variables:
 
     grep -o -P "CAMSHOTBOT_\w+" lib/Telegram/CamshotBot.pm | sort -u
 
-List (useful for Docker):
+Actual List (useful for Docker deployment):
 
     CAMSHOTBOT_CONFIG
     CAMSHOTBOT_DOMAIN
@@ -24,16 +26,37 @@ List (useful for Docker):
 
 To check which variables are set you can run
 
-printenv | grep CAMSHOTBOT\_\* | sort -u
+    printenv | grep CAMSHOTBOT_* | sort -u
+
+For setting environment variable you can use
+
+    export CAMSHOTBOT_POLLING=1
 
 # RUNNING
 
+Docker way
+
+    wget https://raw.githubusercontent.com/pavelsr/camshotbot/master/docker-compose.yml.example > docker-compose.yml
+
+then edit CAMSHOTBOT\_\* variables
+
+    docker-compose up
+
+Standalone way
+
 1) Place .camshotbot file in home user directory or camshotbot.conf.json in directory from what you will run camshotbot
-Add all est
+Add all essential variables:  telegram\_api\_token, stream\_url, bot\_domain
 
-2) As alternative you can set all CAMSHOTBOT\_\* environment variables
+2) As alternative to (1) you can set all CAMSHOTBOT\_\* environment variables (see ENVIRONMENT VARIABLES section)
 
-3) run camshotbot daemon or docker-compose up
+3) run
+
+    camshotbot daemon
+
+You can run ffmpeg in a separate docker container.
+String below will output a single image that is continuously overwritten with new images
+
+docker run -d -it -v $(pwd):/tmp/workdir --network=host jrottenberg/ffmpeg:3.3-alpine -hide\_banner -loglevel error -i rtsp://10.132.193.9//ch0.h264 -f image2 -vf fps=1/3 -y -update 1 latest.jpg
 
 # DEVELOPMENT
 
--- lib/Telegram/CamshotBot.pm
+++ lib/Telegram/CamshotBot.pm
@@ -2,37 +2,15 @@
 
 # ABSTRACT: Telegram bot that send you a snapshot from IP camera using ffmpeg (don't forget to install it!)
 
-=head1 ENVIRONMENT VARIABLES
-
-To get list of all environment variables:
-
-  grep -o -P "CAMSHOTBOT_\w+" lib/Telegram/CamshotBot.pm | sort -u
-
-List (useful for Docker):
-
-  CAMSHOTBOT_CONFIG
-  CAMSHOTBOT_DOMAIN
-  CAMSHOTBOT_FFMPEG_DOCKER
-  CAMSHOTBOT_LAST_SHOT_FILENAME
-  CAMSHOTBOT_POLLING
-  CAMSHOTBOT_STREAM_URL
-  CAMSHOTBOT_TELEGRAM_API_TOKEN
-
-To check which variables are set you can run
-
-  printenv | grep CAMSHOTBOT_* | sort -u
-
-=cut
-
 =head1 RUNNING
 
 Docker way
 
-  wget
+  wget https://raw.githubusercontent.com/pavelsr/camshotbot/master/docker-compose.yml.example > docker-compose.yml
 
-then edit CAMSHOTBOT_* variables
+then edit CAMSHOTBOT_* variables and change network if needed
 
-  docker-compose up
+  docker-compose up -d
 
 Standalone way
 
@@ -45,15 +23,49 @@ Add all essential variables:  telegram_api_token, stream_url, bot_domain
 
   camshotbot daemon
 
+For performance you can run ffmpeg in a separate "caching" docker container.
+String below will output a single image that is continuously overwritten with new images
+
+  docker run -d -it -v $(pwd):/tmp/workdir --network=host jrottenberg/ffmpeg:3.3-alpine -hide_banner -loglevel error -i rtsp://10.132.193.9//ch0.h264 -f image2 -vf fps=1/3 -y -update 1 latest.jpg
+
+For more details please see docker-compose.yml.example
+
+=head1 ENVIRONMENT VARIABLES
+
+Environment variables are always checked firstly, before any config files
+
+To get list of all available environment variables plese run after git clone:
+
+  grep -o -P "CAMSHOTBOT_\w+" lib/Telegram/CamshotBot.pm | sort -u
+
+Actual List (useful for Docker deployment):
+
+  CAMSHOTBOT_CONFIG
+  CAMSHOTBOT_DOMAIN
+  CAMSHOTBOT_FFMPEG_DOCKER
+  CAMSHOTBOT_LAST_SHOT_FILENAME
+  CAMSHOTBOT_POLLING
+  CAMSHOTBOT_STREAM_URL
+  CAMSHOTBOT_TELEGRAM_API_TOKEN
+
+Check more details about their usage at docker-compose.yml.example
+
+To check which variables are set you can run
+
+  printenv | grep CAMSHOTBOT_* | sort -u
+
+For setting environment variable you can use
+
+  export CAMSHOTBOT_POLLING=1
+
 =cut
-
-
 
 package Telegram::CamshotBot;
 
 use Telegram::CamshotBot::Util qw(first_existing_file random_caption abs_path_of_sample_mojo_conf fev);
 use Mojolicious::Lite;
 use Mojolicious::Plugin::JSONConfig;
+use Mojolicious::Plugin::Webtail;
 use WWW::Telegram::BotAPI;
 use Date::Format;
 use Telegram::Bot::Message;
